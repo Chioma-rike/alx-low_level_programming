c4c9efcf3823c1f201570ec6458e5899b6a8673e
Chioma-rike <chiomaezurike89@gmail.com>
2023-09-12 14:20:04 -0700

    Print Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,17 +10,13 @@
  * Description: first struct with alx
  */
 
-/**
- * dog_t - now name for struct dog
- */
-
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
