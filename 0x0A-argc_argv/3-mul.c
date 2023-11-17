75d3b232f277f8baf52a65e2ce1a6a0440ba94c5
chioma-rike <chiomaezurike89@gmail.com>
2023-11-17 13:09:38 -0800

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,67 @@
-#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - Multiples two numbers
- * @argv: Array containing program command line arg
- * @argc: Number of command line
+ * _atoi - Converts a string to an integer
+ * @s: String to be converted
+ * Return: The int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] = '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
+ * main - Multiplies two numbers
+ * @argv: Array containing program command line arguments
+ * @argc: Number of command line arguements
  * Return: (0)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int result, numb1, numb2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	numb1 = _atoi(argv[1]);
+	numb2 = _atoi(argv[2]);
+	result = numb1 * numb2;
+
+	printf("%d\n", result);
+
 	return (0);
 }
